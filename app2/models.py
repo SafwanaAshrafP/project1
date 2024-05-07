2fb3c826621621cde6de3890aefe7941ce527a45
SafwanaAshrafP <safwanaashrafp@gmail.com>
2024-05-07 10:24:34 +0530

    first version

---
+++ app2/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+
+    def _str_(self):
+        return self.username
